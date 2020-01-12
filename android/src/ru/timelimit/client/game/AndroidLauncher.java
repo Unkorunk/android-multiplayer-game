16c4c5bb1832128b8509013d5bdf77bd637449a8
Unkorunk <unkorunk@gmail.com>
2020-01-12 14:14:21 +1000

    init commit

---
+++ android/src/ru/timelimit/client/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.timelimit.client.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.timelimit.client.game.GameClient;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameClient(), config);
+	}
+}
