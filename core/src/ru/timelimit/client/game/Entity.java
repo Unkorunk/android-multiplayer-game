f5b8527093f4b89f59a512664070be010f95f97e
Юрий Михалев <mixalevuriy@gmail.com>
2020-01-15 23:44:13 +1000

    Adding jump

--- core/src/ru/timelimit/client/game/Entity.java
+++ core/src/ru/timelimit/client/game/Entity.java
@@ -6,25 +6,36 @@ public class Entity extends GameObject {
 
     private Vector2 speed = new Vector2(1, 0);
 
+    private int jumpTimer = 0;
+
     @Override
     public void update() {
         BehaviourModel.Command cmd = bm.update();
 
         var curSpeed = new Vector2(speed.x, speed.y);
 
-        // TODO: Normal command handling
-        switch(cmd) {
-            case JUMP:
-                curSpeed.y += 1;
-                break;
+        if (jumpTimer == 0){
+            // TODO: Normal command handling
+            switch(cmd) {
+                case JUMP:
+                    position.y += 25;
+                    jumpTimer = 60;
+                    break;
 
-            case SLIP:
-                curSpeed.y -= 1;
-                break;
+                case SLIP:
+                    // TODO: ???
+                    break;
 
-            default:
-                break;
+                default:
+                    break;
+            }
+        } else {
+            jumpTimer--;
+            if (jumpTimer == 0) {
+                position.y -= 25;
+            }
         }
+
 
         position.x += curSpeed.x;
         position.y += curSpeed.y;
