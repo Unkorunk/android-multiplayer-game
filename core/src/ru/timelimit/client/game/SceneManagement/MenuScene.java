a1651a75e4f6387bad5c962d999e9759e029ecf1
Tina <kviditch@gmail.com>
2020-01-17 16:25:27 +1000

    Menu

--- core/src/ru/timelimit/client/game/SceneManagement/MenuScene.java
+++ core/src/ru/timelimit/client/game/SceneManagement/MenuScene.java
@@ -1,17 +1,38 @@
 package ru.timelimit.client.game.SceneManagement;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import ru.timelimit.client.game.GlobalSettings;
+import ru.timelimit.client.game.TextureManager;
+import ru.timelimit.client.game.UI.MenuUI;
 import ru.timelimit.client.game.UI.UI;
+
+import java.util.Objects;
 
 public class MenuScene implements Scene {
     public int exitCode = 0;
+    private OrthographicCamera camera;
+    private static UI gui = new MenuUI();
+
+    private Sprite background;
 
     @Override
     public void instantiate() {
-        // TODO: implement MenuScene
+
+        float width = Gdx.graphics.getWidth();
+        float height = Gdx.graphics.getHeight();
+
+        camera = new OrthographicCamera(600, 600 * (height / width));
+        camera.position.set(0 + camera.viewportWidth / 2, 0 + camera.viewportHeight / 2, 0);
+        camera.update();
+        gui.init();
+
+        background = new Sprite(Objects.requireNonNull(TextureManager.getTexture("BackgroundSky")));
+
+        background.setSize(camera.viewportWidth * 1.5f, camera.viewportHeight);
+        background.setPosition(camera.viewportWidth * (1.0f - 1.5f) / 2, 0);
     }
 
     @Override
@@ -21,26 +42,24 @@ public class MenuScene implements Scene {
 
     @Override
     public UI getUI() {
-        // TODO: implement MenuScene
-        return null;
+        return gui;
     }
 
     @Override
     public OrthographicCamera getCamera() {
-        // TODO: implement MenuScene
-        return null;
+        return camera;
     }
 
     @Override
     public Sprite getBackground() {
-        return null;
+        return background;
     }
 
     @Override
     public void render(SpriteBatch batch) {
-        // TODO: implement MenuScene
+        background.draw(batch);
+        gui.render(batch);
     }
-
     @Override
     public int isOver() {
         return 0;
